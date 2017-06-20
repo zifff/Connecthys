35a5902b39d0ad092315dd0d01b93c30d0a081ca
zifff <zifff@zifff.com>
2017-06-20 23:21:55 +0200

    test
    test

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 application = Flask(__name__)
 
-@application.route("/")
-def index():
-    return render_template('noconfig.html')
-	
+import os, sys
+REP_CONNECTHYS = os.path.abspath(os.path.dirname(__file__))
+if not REP_CONNECTHYS in sys.path:
+	sys.path.insert(0, REP_CONNECTHYS)
+
+import connecthys.run
 
 if __name__ == "__main__":
     application.run()
