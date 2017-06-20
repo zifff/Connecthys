9c0e09342d6d47c8d4e3adfa09743284bd425011
zifff <zifff@zifff.com>
2017-06-20 22:35:11 +0200

    tt
    tt

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return "Hello World TOTO!"
+def index():
+    return render_template('noconfig.html')
+	
 
 if __name__ == "__main__":
     application.run()
